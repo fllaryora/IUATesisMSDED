12c6b60d67fac4a5b653901fdd6bb2d06f5bdc6f
Cristian Z <cristianzerpa89@gmail.com>
2013-10-23 13:33:51 -0300

    estructuras nodo generico

--- validadorJSON/final/validador.h
+++ validadorJSON/final/validador.h
@@ -2,9 +2,6 @@
 #include <stddef.h>
 #include <wjelement.h>    
 
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
 typedef enum{
 	ERROR_OPEN_JSON,	/* json no existe */
 	ERROR_OPEN_SCHEMA,	/* schema no existe */
@@ -16,31 +13,102 @@ typedef enum{
 	VALIDATION_PASS		/* validacion correcta */
 }VALIDATE;
 
-#endif
+typedef struct{
+	char *distribution;
+	double least;
+	double highest;
+	double constant;
+	double mean;
+	double variance;
+	double lambda;
+	double mode;
+	double minimun;
+	double maximun;
+	double shapeAlpha;
+	double shapeBeta;
+	double shape;
+	double escale;
+	int seed;
+}Delay;
 
+typedef struct{
+    int idNode;
+    int resource;
+    int *preceders; /*primer elemento va a ser la cantidad >=0*/
+    int *followers;
+    double fixedCost;
+    double variableCost;
+}Queue;
 
-/* 
-double        json_object_dotget_number (const JSON_Object *object, const char *name);
-int           json_object_dotget_boolean(const JSON_Object *object, const char *name);
+typedef struct{
+    int idNode;
+    int quantity;
+    int *preceders;
+    int *followers;
+}Counter;
 
-Functions to get available names 
-size_t        json_object_get_count(const JSON_Object *object);
-const char  * json_object_get_name (const JSON_Object *object, size_t index);
-    
-JSON_Value  * json_array_get_value  (const JSON_Array *array, size_t index);
-const char  * json_array_get_string (const JSON_Array *array, size_t index);
-JSON_Object * json_array_get_object (const JSON_Array *array, size_t index);
-JSON_Array  * json_array_get_array  (const JSON_Array *array, size_t index);
-double        json_array_get_number (const JSON_Array *array, size_t index);
-int           json_array_get_boolean(const JSON_Array *array, size_t index);
-size_t        json_array_get_count  (const JSON_Array *array);
+typedef struct{
+	int idNode;
+    int input;
+	int output;
+    int *preceders;
+    int *followers;
+    double *probabilisticBranch; /*primer elemento poner cero si no tiene elementos aunque sea double*/
+}Function;
 
-JSON_Value_Type json_value_get_type   (const JSON_Value *value);
-JSON_Object *   json_value_get_object (const JSON_Value *value);
-JSON_Array  *   json_value_get_array  (const JSON_Value *value);
-const char  *   json_value_get_string (const JSON_Value *value);
-double          json_value_get_number (const JSON_Value *value);
-int             json_value_get_boolean(const JSON_Value *value);*/
+typedef struct{
+    int idNode;
+	int *preceders;
+	int *followers;
+    double *probabilisticBranch;
+	Delay delay;
+}Normal;
+
+typedef struct{
+	int idNode;
+	int *preceders;
+	int *followers;
+    double *probabilisticBranch;
+	Delay delay;
+}Combi;
+
+/*	Delay
+	char *distribution; /*uniform/
+	double least
+	double highest
+	int seed
+
+	char *distribution; /*deterministic/
+	double constant;
+
+	char *distribution; /*normal/
+	double mean;
+	double variance;
+	int seed
+
+	char *distribution; /*exponential/
+	double lambda;
+	int seed
+
+	char *distribution; /*triangular/
+	double least
+	double highest
+	double mode
+	integer seed
+
+	char *distribution; /*beta/
+	double minimun;
+	double maximun;
+	double shapeAlpha;
+	double shapeBeta
+	int seed;
+
+	char *distribution; /*log-normal/
+	double escale
+	double shape
+	double minimun
+	int seed
+*/
 int					validateSchema(const char *filenameJson , const char *filenameSchema);
 static void			schema_error(void *client, const char *format, ...);
 static WJElement	schema_load(const char *name, void *client, const char *file, const int line);
@@ -49,3 +117,4 @@ void getArray(JSON_Object *object, const char *arregloJson,const char *atributeJ
 void getArrayInArray(JSON_Object * objectJson,const char *arrayJson,int pos,const char *arrayJsonIn, int** arreglo, int* m);
 int countArrayInclude(int **array,int sizeArray,int **arrayFull, int sizeArrayFull);
 int repeatArrays(int **array1 ,int sizeArray1, int **array2,int sizeArray2, int **array3, int sizeArray3, int **array4, int sizeArray4, int **array5, int sizeArray5,int** array, int* sizeArray);
+
