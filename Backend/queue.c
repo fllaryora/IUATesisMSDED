f25d5a2f3c1f4e680e08633f62842e4873d15323
Francisco Llaryora <francisco.llaryora@mercadolibre.com>
2013-11-10 21:56:51 -0300

    cambio sobre queue

--- Backend/queue.c
+++ Backend/queue.c
@@ -3,6 +3,25 @@
 
 
 void queueNode( const  Queue *initialStatus){
+	//cantidad de deltaT que pasaron en este tiempo
+	unsigned long long int deltaTCount = 0;
+	//numero de recursos en espera acumulados
+	unsigned long long int delatedResourcesAccum = 0;
+	unsigned long long int inputs = 0;
+	unsigned long long int outputs = 0;
+	int maximun = initialStatus->resource;
+	int minimun = initialStatus->resource;
+
+	//TODO booleano de si el nodo esta terminado
+	//TODO lista current sobre las combis activas
+	//TODO contador de delta T no vacios
+	//TODO nivel: cantidad de recursos disponibles
+
+
+	//TODO contador de delta T no vacios / contador de delta T
+	//TODO recursoss en espera acumulados / delta t
+
+	//TODO recursos pendientes para pasar al cuerpo
 
 	return;
 }
