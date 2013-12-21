3e0787ced52c70ef7689a6159f24a9e5fcd351a2
Francisco Llaryora <francisco.llaryora@mercadolibre.com>
2013-12-21 02:10:24 -0300

    se arreglo beta

--- Test/TU_RNG/gammatest.c
+++ Test/TU_RNG/gammatest.c
@@ -4,34 +4,57 @@
 #include "RNGs.h"
 
 #define N 1000
-#define M 100
+#define M 50
+
+// int main(int argc,char **argv)
+// {
+//    int i;
+//    double r,rmin=1e32,rmax=-1e32;
+//    int bins[N+1];
+//    double sum=0,sum2=0;
+
+//    for (i=0;i<N;i++)
+//       bins[i] = 0;
+
+//    //RandomInitialise(1802,9373);
+//    for (i=0;i<M;i++) {
+//       r = RandomGammaInteger(10, 2.0);
+//       sum += r;
+//       printf("%g \n",r);
+//       sum2 += (r*r);
+//       int entero = (int)r;
+//       /* printf("%g\n",r); */
+//       if (r >= 0.0 && r <  1000.0)
+// 		bins[entero]++;
+		
+//    }
+//   // for (i=0;i<N;i++)
+//     //  printf("%d %d\n",i,bins[i]);
+   
+//    fprintf(stderr,"Mean = %g\n",sum / M);
+//    fprintf(stderr,"Standard deviation = %g\n",sqrt((sum2 - sum*sum/M)/M));
+
+// }
+
 
 int main(int argc,char **argv)
 {
    int i;
-   double r,rmin=1e32,rmax=-1e32;
-   int bins[N+1];
-   double sum=0,sum2=0;
-
-   for (i=0;i<N;i++)
-      bins[i] = 0;
-
+   double c;
+   double sum = 0.0;
    //RandomInitialise(1802,9373);
    for (i=0;i<M;i++) {
-      r = RandomGammaInteger(10, 2.0);
-      sum += r;
-      printf("%g \n",r);
-      sum2 += (r*r);
-      int entero = (int)r;
-      /* printf("%g\n",r); */
-      if (r >= 0.0 && r <  1000.0)
-		bins[entero]++;
-		
+      c =  RandomGamma( 0.0, 1.0 , 0.6 );
+      sum += c;
    }
-  // for (i=0;i<N;i++)
-    //  printf("%d %d\n",i,bins[i]);
-   
-   fprintf(stderr,"Mean = %g\n",sum / M);
-   fprintf(stderr,"Standard deviation = %g\n",sqrt((sum2 - sum*sum/M)/M));
+   printf("la media de gamma es  a+ b*c (en este caso 2) %lf\n", sum/M);
+   /*++
+este caso 2) 1.901096
+[francisco@lechuza TU_RNG]$ ./GMM
+la media de gamma es  a+ b*c (en este caso 2) 2.273964
+[francisco@lechuza TU_RNG]$ ./GMM
+la media de gamma es  a+ b*c (en este caso 2) 1.911473
 
+
+   +*/
 }
