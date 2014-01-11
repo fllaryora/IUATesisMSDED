ea4b02178122da38780f9ae8ba9d9c282d008db6
fllaryora <francisco.llaryora@mercadolibre.com>
2014-01-11 20:01:21 -0300

    merge al master

--- Backend/compileValgrind.sh
+++
@@ -1,9 +0,0 @@
-COMPILER="gcc"
-GCCARGS="-Wall -O0 -g -std=c99"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-$COMPILER -Wall $GCCARGS -c parson.c
-$COMPILER -Wall $GCCARGS -c jsonHelperN.c
-$COMPILER -Wall $GCCARGS -lwjelement -lwjreader parson.o jsonHelperN.o jsonHelperTest.c -o testJson
-
-valgrind -v ./testJson
-rm *.o
